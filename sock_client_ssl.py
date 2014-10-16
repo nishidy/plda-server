7c849429873385ddda00a83b29c1a43a22b61655
nishidy <nishidy@localhost.localdomain>
2014-10-16 19:03:27 +0900

    SSL is supported. Client code for SSL is added, too.
    I wont update correct patch file anymore and try to commit all files intead.

--- sock_client_ssl.py
+++ sock_client_ssl.py
@@ -2,9 +2,9 @@
 
 import sys
 import socket
+import ssl
 from optparse import OptionParser
 from time import time
-
 
 def main():
 
@@ -14,6 +14,7 @@ def main():
 	parser.add_option("-i","--dest_ip",dest="dest_ip",type="string",default="127.0.0.1",help="connect to DEST_IP")
 	parser.add_option("-p","--dest_port",dest="dest_port",type="int",default=5328,help="connect to DEST_PORT")
 	parser.add_option("-v","--verbose",action="store_true",default=False,help="verbose")
+	parser.add_option("-c","--cert_file",dest="cert_file",help="Server certification file", metavar="FILE")
 
 	(options, args) = parser.parse_args()
 
@@ -25,29 +26,32 @@ def main():
 	port   = options.dest_port
 
 	clientsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	clientsock.connect((server,port))
+	ssl_clientsock = ssl.wrap_socket(clientsock)
+
+	ssl_clientsock.connect((server,port))
 
 	rcvbuf = 1024
 	for line in open(options.filename,"r"):
 
 		stime=float(time())
 		
-		clientsock.sendall(line)
+		ssl_clientsock.sendall(line)
 
 		rcvmsg=""
 		while True:
-			tmpmsg = clientsock.recv(rcvbuf)
+			tmpmsg=ssl_clientsock.recv(rcvbuf)
 			rcvmsg += tmpmsg
 			if len(tmpmsg) < rcvbuf: break
 
 		etime=float(time())
 
 		if options.verbose:
+			print "Received from server[%s] (# of topics is %d): %s" %\
+					(server,len(rcvmsg.split()),rcvmsg)
 			print "%.3f sec elapsed to get the result." % (etime-stime)
-			print "Received from server[%s] (# of topics is %d): %s" % (server,len(rcvmsg.split()),rcvmsg)
 
 
-	clientsock.close()
+	ssl_clientsock.close()
 
 
 if __name__ == "__main__":
